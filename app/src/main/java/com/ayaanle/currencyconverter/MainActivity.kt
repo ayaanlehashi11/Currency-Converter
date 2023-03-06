e8e58ef9e75fde09a607a14422feadfe1287283f
ayanle <ayaanlemohamed540@gmail.com>
2023-03-06 16:00:25 +0300

    init

--- app/src/main/java/com/ayaanle/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/ayaanle/currencyconverter/MainActivity.kt
@@ -14,19 +14,16 @@ class MainActivity : AppCompatActivity() {
     var convert:Button? = null
     var swap:Button? = null
     lateinit var toolbar : Toolbar
-    lateinit var utils: Utils
     private lateinit var builder:AlertDialog.Builder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toolbar = findViewById<Toolbar>(R.id.toolbar) as Toolbar
-        toolbar.title = "Currency Converter"
-        setSupportActionBar(toolbar)
+
         if (convert != null)
         {
             convert?.setOnClickListener {
                 Toast.makeText(applicationContext , "Converting..." , Toast.LENGTH_SHORT).show();
-                utils.dialog_initializer().show()
+               // utils.dialog_initializer().show()
 
             }
         }
