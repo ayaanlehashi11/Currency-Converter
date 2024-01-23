9677e79f802233345d27155f01b94ff47c7a7e6e
ayaanlehashi11 <ayaanlemohamed540@gmail.com>
2024-01-22 22:32:16 -0800

    symbol enum and aggregators

---
+++ app/src/main/java/com/ayaanle/currencyconverter/Utils.java
@@ -0,0 +1,24 @@
+package com.ayaanle.currencyconverter;
+
+public class Utils
+{
+    public String symbol;
+    public enum SYMBOL
+    {
+        EUR,
+        GBP,
+        USD,
+        JPY,
+        CHF,
+    }
+
+    public String getSymbol()
+    {
+        return " ";
+    }
+
+    public void setSymbol(String symbol)
+    {
+
+    }
+}
